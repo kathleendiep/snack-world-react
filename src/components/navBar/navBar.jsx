4ab72be5ae4a0a1b6053156e90a8875621f6d614
kathleendiep <kathleendiep@gmail.com>
2022-04-14 13:00:58 -0700

    fixed nav

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -2,53 +2,49 @@ import React from 'react'
 import { useState } from 'react'
 import { Link } from "react-router-dom";
 import './navBar.css'
-import {  faNavicon } from "@fortawesome/free-solid-svg-icons";
+import { faNavicon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const NavBar = () => {
-   // default - false, will show bar icon
-   // true will show the whole thing
-   const [showing, setShowing] = useState(false)
-   const toggleShowing = () => {
-     // sets it as true and will show 
-       setShowing(!showing)
-   }
-    return(
-      // FIX THIS UP
-      <>
-        {
-          // if showing is true - it will show the items
-            showing
-            ?
-              <nav class="navbar navbar-default" role="navigation">
-                <div id="container">
-                   <h1 className="name-title"><Link to="/">Snack World</Link></h1> 
-                    <nav id="hamburgerNav">
-                      <button onClick={toggleShowing} class="fa fa-bars open" aria-hidden="true"><FontAwesomeIcon icon={faNavicon} /></button>
-                    </nav>
-                </div> 
-                <div class="container">
-                    <nav id="sideNav">
-                      <div>
-                        {/* Endpoint to route to Home component */}
-                        <Link to="/">Home</Link>
-                        {/* Endpoint to route to About component */}
-                        <Link to="/about">About</Link>
-                        {/* Endpoint to route to Check more out! component */}
-                        <Link to="/adventures">Check out some!</Link>
-                      </div> 
-                    </nav>
-                </div>
-            </nav>
-            :
-            <div id="container">
-                   <h1 className="name-title"><Link to="/">Snack World</Link></h1> 
-                <nav id="hamburgerNav">
-                  <button onClick={toggleShowing} class="fa fa-bars open" aria-hidden="true"><FontAwesomeIcon icon={faNavicon} /></button>
-                </nav>
-             </div> 
-            }
-        </>
-    )
+  // default - false, will show bar icon
+  // true will show the whole thing
+  const [showing, setShowing] = useState(false)
+  const toggleShowing = () => {
+    // sets it as true and will show 
+    setShowing(!showing)
+  }
+  return (
+
+    <>
+      <div class="nav-header">
+        <div class="nav-brand">
+          <img src="https://s3-us-west-2.amazonaws.com/parallax-theme/assets/bradlogo2.png"></img>
+        </div>
+        <i class="fa fa-bars fa-3x"></i>
+        <div class="header-links">
+          <ul>
+            <li data-menuanchor="fourthPage"><a href="#contact">CONTACT</a></li>
+            <li data-menuanchor="thirdPage"><a href="#portfolio">PORTFOLIO</a></li>
+            <li data-menuanchor="secondPage"><a href="#about">ABOUT</a></li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default NavBar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
