126a190469794a3e3937cfe4ee77a1a0bfe4020d
kathleendiep <kathleendiep@gmail.com>
2022-04-14 16:53:08 -0700

    Added footer back

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { useState } from 'react'
+import './footer.scss'
 import {  faNavicon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
     return(
-        <h1>made by Kathleen</h1>
+        <footer>
+            <div className='wrapper'>
+            <ul>
+                <li class="facebook"><a href="#"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a></li>
+                <li class="twitter"><a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a></li>
+                <li class="instagram"><a href="#"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a></li>
+                <li class="google"><a href="#"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a></li>
+                <li class="whatsapp"><a href="#"><i class="fa fa-laptop fa-2x" aria-hidden="true"></i></a></li>
+            </ul>
+            <p>&copy;2022 Kathleen Diep | All Rights Reserved</p>
+            </div>
+        </footer> 
     )
 }
 
